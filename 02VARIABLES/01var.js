4b1892847415552c4f34759555a5a59231b6708e
Rig6y <roma66331@fmail.com>
2021-12-21 19:40:21 +0100

    commit

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,18 @@
+var x = 4;
+var y = 6;
+var z = x + y;
+
+var name = "Roman";
+
+var person = "Karolina Kowalska", carName = "Nissan", price = 20000;
+
+var personA = "Karolina Kowalska",
+carNameA = "Nissan",
+priceA = 20000;
+
+var carNameB;
+var personX = "Janina" + " " + "Nowak";
+
+var isFamous = true; //false
+
+var xx = 2 + 3 + "5";
